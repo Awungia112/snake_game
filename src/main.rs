faa0f4d8598b5bbc0f067c0149a71666436b4bfc
Awungia112 <awungiaf@gmail.com>
2025-03-08 10:17:59 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+extern crate piston_window;
+extern crate rand;
+
+mod draw;
+mod game;
+mod snake;
+
+use piston_window::types::Color;
+use piston_window::*;
+
+use draw::to_coord;
+use game::Game;
+
+const BACK_COLOR: Color = [0.5, 0.5, 0.5, 1.0];
+
+fn main() {
+    let (width, height) = (20, 20);
+
+    let mut window: PistonWindow = WindowSettings::new("Snake", [to_coord(width), to_coord(height)])
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+        
+    let mut game = Game::new(width, height);
+
+    while let Some(event) = window.next() {
+        if let Some(Button::Keyboard(key)) = event.press_args() {
+            game.key_pressed(key); 
+        }
+
+        // Update game state outside of the drawing closure
+        event.update(|arg| {
+            game.update(arg.dt);
+        });
+
+        // Draw the game state
+        window.draw_2d(&event, |c, g, _| {
+            clear(BACK_COLOR, g);
+            game.draw(&c, g);
+        });
+    }
+}
